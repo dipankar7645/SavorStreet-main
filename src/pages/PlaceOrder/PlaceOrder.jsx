7f1dbccc0b92fe01275253b238aab12447568ef6
dipankar7645 <dipankar11092003@gmail.com>
2025-05-07 15:23:09 +0530

    done

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,32 +1,74 @@
 import { useContext } from "react"
 import "./PlaceOrder.css"
 import { StoreContext } from "../../context/StoreContext"
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom"
 
 const PlaceOrder = () => {
 
-  const { getTotalCartAmount } = useContext(StoreContext);
-
-  const navigate = useNavigate();
+  const { getTotalCartAmount } = useContext(StoreContext)
+  const navigate = useNavigate()
 
   return (
     <form className="place-order">
       <div className="place-order-left">
         <p className="title">Delivery Information</p>
+
         <div className="multi-fields">
-          <input type="text" placeholder="first name" />
-          <input type="text" placeholder="last name" />
+          <input type="text" placeholder="First name" />
+          <input type="text" placeholder="Last name" />
         </div>
+
         <input type="email" placeholder="Email address" />
         <input type="text" placeholder="Street" />
+
         <div className="multi-fields">
+          <select>
+            <option value="">Select State</option>
+            <option value="Andhra Pradesh">Andhra Pradesh</option>
+            <option value="Arunachal Pradesh">Arunachal Pradesh</option>
+            <option value="Assam">Assam</option>
+            <option value="Bihar">Bihar</option>
+            <option value="Chhattisgarh">Chhattisgarh</option>
+            <option value="Goa">Goa</option>
+            <option value="Gujarat">Gujarat</option>
+            <option value="Haryana">Haryana</option>
+            <option value="Himachal Pradesh">Himachal Pradesh</option>
+            <option value="Jharkhand">Jharkhand</option>
+            <option value="Karnataka">Karnataka</option>
+            <option value="Kerala">Kerala</option>
+            <option value="Madhya Pradesh">Madhya Pradesh</option>
+            <option value="Maharashtra">Maharashtra</option>
+            <option value="Manipur">Manipur</option>
+            <option value="Meghalaya">Meghalaya</option>
+            <option value="Mizoram">Mizoram</option>
+            <option value="Nagaland">Nagaland</option>
+            <option value="Odisha">Odisha</option>
+            <option value="Punjab">Punjab</option>
+            <option value="Rajasthan">Rajasthan</option>
+            <option value="Sikkim">Sikkim</option>
+            <option value="Tamil Nadu">Tamil Nadu</option>
+            <option value="Telangana">Telangana</option>
+            <option value="Tripura">Tripura</option>
+            <option value="Uttar Pradesh">Uttar Pradesh</option>
+            <option value="Uttarakhand">Uttarakhand</option>
+            <option value="West Bengal">West Bengal</option>
+            <option value="Andaman and Nicobar Islands">Andaman and Nicobar Islands</option>
+            <option value="Chandigarh">Chandigarh</option>
+            <option value="Dadra and Nagar Haveli and Daman and Diu">Dadra and Nagar Haveli and Daman and Diu</option>
+            <option value="Delhi">Delhi</option>
+            <option value="Jammu and Kashmir">Jammu and Kashmir</option>
+            <option value="Ladakh">Ladakh</option>
+            <option value="Lakshadweep">Lakshadweep</option>
+            <option value="Puducherry">Puducherry</option>
+          </select>
           <input type="text" placeholder="City" />
-          <input type="text" placeholder="State" />
         </div>
+
         <div className="multi-fields">
           <input type="text" placeholder="Zip Code" />
           <input type="text" placeholder="Country" />
         </div>
+
         <input type="text" placeholder="Phone" />
       </div>
 
@@ -47,7 +89,7 @@ const PlaceOrder = () => {
             <p>Total</p>
             <p>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</p>
           </div>
-          <button onClick={()=> navigate("/successful")}>PROCEED TO PAYMENT</button>
+          <button type="button" onClick={() => navigate("/successful")}>PROCEED TO PAYMENT</button>
         </div>
       </div>
     </form>
